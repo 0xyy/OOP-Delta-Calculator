b0c4da21bcc82c17c458c1220c36b02c29e01bef
Tommy Phan <70032717+0xyy@users.noreply.github.com>
2021-08-07 00:57:54 +0200

    refactor delta calculator OOP

--- code.js
+++ code.js
@@ -1,80 +1,36 @@
-// POBIERZ ELEMENTY Z STRONY
-const aValue = document.getElementById('aValue');
-const bValue = document.getElementById('bValue');
-const cValue = document.getElementById('cValue');
-const submitButton = document.getElementById('submitButton');
-const deltaWynik = document.getElementById('delta');
-const mzWynik = document.getElementById('miejscaZerowe');
-const wierzchołekWynik = document.getElementById('wierzchołek');
-const ogólnaWynik = document.getElementById('ogólna');
-const kanonicznaWynik = document.getElementById('kanoniczna');
-const iloczynowaWynik = document.getElementById('iloczynowa');
+const aInput = document.getElementById('a-value');
+const bInput = document.getElementById('b-value');
+const cInput = document.getElementById('c-value');
+const submitButton = document.getElementById('submit-button');
+const deltaResultEl = document.getElementById('delta');
+const rootsEl = document.getElementById('roots');
+const apexEl = document.getElementById('apex');
+const generalFormEl = document.getElementById('general-form');
+const canonicalFormEl = document.getElementById('canonical-form');
 
-const makeCalculations = (e) => {
+const showResults = e => {
     e.preventDefault();
-    // POBIERZ AKTUALNE WARTOSCI A B I C
-    let activeA = aValue.value;
-    let activeB = bValue.value;
-    let activeC = cValue.value;
-    
-    // OBLICZANIE DELTY
-    const delta = obliczDelte(activeA, activeB, activeC);
-    obliczMZ(delta);
 
-    // OBLICZANIE P I Q
-    const p = obliczP(activeA, activeB);
-    const q = obliczQ(activeA, delta);
+    const aValue = +aInput.value;
+    const bValue = +bInput.value;
+    const cValue = +cInput.value;
 
+    try {
+        const delta = new Delta(aValue, bValue, cValue);
 
-    // WARUNKI DO POSTACI OGÓLNEJ
-    if (activeB < 0 && activeC < 0) {
-        ogólnaWynik.textContent = `y = ${activeA}x² - ${-activeB}x - ${-activeC}`;
-    } else if (activeB >= 0 && activeC < 0) {
-        ogólnaWynik.textContent = `y = ${activeA}x² + ${activeB}x - ${-activeC}`;
-    } else if (activeB < 0 && activeC >= 0) {
-        ogólnaWynik.textContent = `y = ${activeA}x² - ${-activeB}x + ${activeC}`;
-    } else if (activeB >= 0 && activeC >= 0) {
-        ogólnaWynik.textContent = `y = ${activeA}x² + ${activeB}x + ${activeC}`;
+        deltaResultEl.textContent = delta.getDelta();
+        rootsEl.textContent = delta.getRoots();
+        apexEl.textContent = delta.getVertexOfAParabola();
+        generalFormEl.textContent = delta.getGeneralForm();
+        canonicalFormEl.textContent = delta.getCanonicalForm();
+    } catch (err) {
+        alert(err.name + ': ' + err.message);
     }
 
-    // WYŚWIETLANIE WIERZCHOŁKÓW PARABOLI
-    wierzchołekWynik.textContent = `(${p}, ${q})`;
 
-    // WARUNKI DO POSTACI KANONICZZNEJ
-    if (p < 0 && q < 0) {
-        kanonicznaWynik.textContent = `y = ${activeA}(x + ${-p})² + ${q}`;
-    } else if (p >= 0 && q < 0) {
-        kanonicznaWynik.textContent = `y = ${activeA}(x - ${p})² + ${q}`;
-    } else if (p < 0 && q >= 0) {
-        kanonicznaWynik.textContent = `y = ${activeA}(x + ${-p})² + ${q}`;
-    } else if (p >= 0 && q >= 0) {
-        kanonicznaWynik.textContent = `y = ${activeA}(x - ${p})² + ${q}`;
-    }
+    aValue.value = '';
+    bValue.value = '';
+    cValue.value = '';
 }
 
-const obliczDelte = (a, b, c) => {
-    const delta = b**2 - 4 * a * c;
-    return deltaWynik.textContent = delta;
-}
-
-const obliczMZ = delta => {
-    if (delta < 0) {
-        return mzWynik.textContent = '0 miejsc zerowych';
-    } else if (delta === 0) {
-        return mzWynik.textContent = '1 miejsce zerowe';
-    } else if (delta > 1) {
-        mzWynik.textContent = '2 rózne miejsca zerowe';
-    }
-}
-
-const obliczP = (a, b) => {
-    const p = (- b / (2 * a));
-    return p;
-}
-
-const obliczQ = (a, delta) => {
-    const q = (- (delta) / (4 * a));
-    return q;
-}
-
-submitButton.addEventListener('click', makeCalculations)
+submitButton.addEventListener('click', showResults);
---
+++ delta.js
@@ -0,0 +1,74 @@
+class Delta {
+    #aValue;
+
+    #bValue;
+
+    #cValue;
+
+    #p;
+
+    #q;
+
+    constructor(aValue, bValue, cValue) {
+        if (aValue == '' || bValue == '' || cValue == '') {
+            throw new TypeError('Enter a number!');
+        }
+
+        this.#aValue = aValue;
+        this.#bValue = bValue;
+        this.#cValue = cValue;
+        this.#p = (- this.#bValue / (2 * this.#aValue));
+        this.#q = (- (this.getDelta()) / (4 * this.#aValue));
+    }
+
+    getDelta() {
+        return this.#bValue**2 - 4 * this.#aValue * this.#cValue;
+    }
+
+    getRoots() {
+        if (this.getDelta() < 0) {
+            return '0 miejsc zerowych';
+        } else if (this.getDelta() === 0) {
+            return '1 miejsce zerowe';
+        } else if (this.getDelta() > 1) {
+            return '2 rózne miejsca zerowe';
+        }
+    }
+
+    getVertexOfAParabola() {
+        return `(${this.#p}, ${this.#q})`;
+    }
+
+    getGeneralForm() {
+        if (this.#bValue < 0 && this.#cValue < 0) {
+            return `y = ${this.#aValue}x² - ${-this.#bValue}x - ${-this.#cValue}`;
+        } else if (this.#bValue >= 0 && this.#cValue < 0) {
+            return `y = ${this.#aValue}x² + ${this.#bValue}x - ${-this.#cValue}`;
+        } else if (this.#bValue < 0 && this.#cValue >= 0) {
+            return`y = ${this.#aValue}x² - ${-this.#bValue}x + ${this.#cValue}`;
+        } else if (this.#bValue >= 0 && this.#cValue >= 0) {
+            return `y = ${this.#aValue}x² + ${this.#bValue}x + ${this.#cValue}`;
+        }
+    }
+
+    getCanonicalForm() {
+        if (this.#p < 0 && this.#q < 0) {
+            return `y = ${this.#aValue}(x + ${-this.#p})² + ${this.#q}`;
+        } else if (this.#p >= 0 && this.#q < 0) {
+            return `y = ${this.#aValue}(x - ${this.#p})² + ${this.#q}`;
+        } else if (this.#p < 0 && this.#q >= 0) {
+            return`y = ${this.#aValue}(x + ${-this.#p})² + ${this.#q}`;
+        } else if (this.#p >= 0 && this.#q >= 0) {
+            return`y = ${this.#aValue}(x - ${this.#p})² + ${this.#q}`;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
